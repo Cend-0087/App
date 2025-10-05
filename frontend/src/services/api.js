a858137c215946713b4ad5e9c36ecda084d763f8
Матвей <matvey.6420@gmail.com>
2025-10-05 21:33:18 +0300

    fix img

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Автоматически использовать Render API, если сайт запущен в продакшене
 const API_BASE =
-  import.meta.env.VITE_API_URL ||
-  (window.location.hostname.includes("onrender.com")
-    ? "https://app-z8kj.onrender.com/api"
-    : "http://localhost:4000/api");
+  import.meta.env.VITE_API_URL || "https://app-z8kj.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
