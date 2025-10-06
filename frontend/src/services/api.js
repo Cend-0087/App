46694f064db5fd6e1c6787bdd3820d331cb7d23a
Матвей <matvey.6420@gmail.com>
2025-10-06 22:19:31 +0300

    final api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
+
+// Жестко задать URL. Для локалки.
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:4000/api',
+// });
+
+// Для деплоя
 
 const api = axios.create({
   baseURL: "https://app-z8kj.onrender.com/api"
 });
 
 console.log("🔥 Использую продакшн API");
+
 
 export default api;
